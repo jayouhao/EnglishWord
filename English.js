fe03d7cd7d1ab444b6fc3b92b39a56ba36af612c
home <2991944127@qq.com>
2019-07-10 19:36:51 +0800

    create

---
+++ English.js
@@ -0,0 +1,155 @@
+address			/ə'dres/			地址
+
+panel			/'pæn(ə)l/			仪表板
+
+reveal			/rɪ'viːl/			显示
+
+tab				/tæb/				标签
+
+important		/ɪm'pɔːt(ə)nt/		重要的
+
+server			/'sɜːvə/			服务器
+
+download		/daʊn'ləʊd/			下载
+
+using			/u'sɪŋ/				使用
+
+start			/stɑːt/				开始
+
+directly		/dɪ'rektlɪ/			直接
+
+will			/wɪl/				将
+
+shortly			/'ʃɔːtlɪ/			立刻
+
+you				/juː/				你
+
+help			/help/				帮助
+
+route			/ruːt/				路线
+
+router			/'ruːtə(r)/			路由器
+
+member			/'membə/			成员
+
+views			/v'ju:z/			视图
+
+word			/wɜːd/				单词
+
+break			/breɪk/				打断
+
+accept			/əkˈsept/			接受
+	
+the				/te/				这
+
+agreement		/ə'griːm(ə)nt/		协议，同意
+
+next			/nekst/				下一步
+
+browse			/braʊz/				浏览
+
+cancel			/'kæns(ə)l/			取消
+
+back			/bæk/ 				后退
+
+finish			/'fɪnɪʃ/			完成
+
+reduce			/rɪ'djuːs/			减少
+
+add 			/æd/				增加
+
+unknown			/ʌn'nəʊn/			未知
+
+uncaught		/u'ka g/ 			未捕获
+
+mismatched		/mɪs'mætʃ/			不匹配
+
+module			/'mɒdjuːl/			模块
+
+anonymous		/ə'nɒnɪməs/			匿名
+
+require			/rɪ'kwaɪə/			需求
+
+request			/rɪ'kwest/			请求
+
+response		/rɪ'spɒns/			响应
+
+license			/'laɪsns/			许可证
+
+source			/sɔːs/				来源
+
+origin			/'ɒrɪdʒɪn/			起源
+
+resource		/rɪ'sɔːs;/			资源
+
+static			/'stætɪk/			静态的
+
+define			/dɪ'faɪn/			定义
+
+execute			/'eksɪkjuːt/ 		执行
+
+focus			/'fəʊkəs/			聚焦
+
+build			/bɪld/				建立
+
+price			/praɪs/				价格
+
+print			/prɪnt/				打印
+
+boxed			/bɑkst/				盒子
+
+upload			/ʌp'ləʊd/			加载，上传
+
+amend			/ə'mend/			修改
+
+restart			/riː'stɑːt/ 		重启
+
+all				/ɔːl/				全部
+
+think			/θɪŋk/				想 
+
+access			/'ækses/			访问
+
+from			/frɒm/				来自
+
+form 			/fɔːm/				表格 
+
+find			/faɪnd/				查找
+
+prev			/priv/				上一个
+
+present			/prɪˈzent/			现在
+
+on 				/ɒn/				在
+
+control			/kən'trəʊl/			管理
+
+allow			/ə'laʊ/				允许
+
+blocked			/blɒkt/				阻塞
+
+has				/hæz/				有
+
+been			/biːn/				是
+
+has been							已经
+
+to				/tu/				位于
+
+at  			/æt/				在
+
+center			/ˈsentə(r)/			中心
+
+append			/ə'pend/			附加
+
+detail			/'diːteɪl/			详情
+
+location		/lo'keʃən/			位置
+
+href			/he'f/				超链接
+
+history			/'hɪst(ə)rɪ/		历史
+
+core			/kɔː/				核心
+
+examine			/ɪg'zæmɪn/			查看，检查
